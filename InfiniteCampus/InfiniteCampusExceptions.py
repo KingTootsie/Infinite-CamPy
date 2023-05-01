b97699c65bb4af2af9f5d50ab74d8a96eb91c563
King Tootsie <kingtootsietechnology@protonmail.com>
2023-04-30 22:42:08 -0500

    Converted IC to actual library & added features

--- InfiniteCampus/InfiniteCampusExceptions.py
+++ InfiniteCampus/InfiniteCampusExceptions.py
@@ -8,6 +8,7 @@ class LoginExceptions:
         def __init__(self, message = "A user has already been logged into on this instance.", *args: object) -> None:
             super().__init__(message, *args)
             self.message = message
+            
     class InvalidUsernameOrPassword(Exception):
         def __init__(self, message = "Incorrect username or password provided.", *args: object) -> None:
             super().__init__(message, *args)
@@ -15,5 +16,11 @@ class LoginExceptions:
 
     class NoDistrictFound(Exception):
         def __init__(self, message = "No district was found with queries provided.", *args: object) -> None:
+            super().__init__(message, *args)
+            self.message = message
+
+class AuthorizationExceptions:
+    class SessionHasExpired(Exception):
+        def __init__(self, message = "This session has expired because it has gone 1 hour without activity.", *args: object) -> None:
             super().__init__(message, *args)
             self.message = message
--- InfiniteCampus/__init__.py
+++ InfiniteCampus/__init__.py
@@ -1,35 +1,52 @@
 import requests
-import login
 import datetime
-import InfiniteCampusExceptions
+import time
+from . import InfiniteCampusExceptions
 
+#NOTE: Sessions expire after 1 hour of inactivity
 class InfiniteCampus():
     def __init__(self) -> None:
         self.session = requests.session()
         self.district_data = None
         self.logged_in = False
+        self.last_interaction_timestamp = None
         self.Student = InfiniteCampus.Student(infinite_campus_self=self)
         self.Grades = InfiniteCampus.Grades(infinite_campus_self=self)
         self.Calendar = InfiniteCampus.Calendar(infinite_campus_self=self)
+
+    def _authentication_check(self):
+        current_time = time.time()
+
+        if self.last_interaction_timestamp is None:
+            self.last_interaction_timestamp = current_time
+            return
+        
+        if current_time >= (self.last_interaction_timestamp + 3600):
+            self.session.cookies.clear()
+            self.logged_in = False
+            self.district_data = None
+            raise InfiniteCampusExceptions.AuthorizationExceptions.SessionHasExpired
+        else:
+            self.last_interaction_timestamp = current_time
     
-        """
-        Logs into a student campus portal.
+    """
+    Logs into a student campus portal.
 
-        Inputs:
-        - username (string): The username for the student.
-        - password (string): The password for the student.
-        - district_name (string): This is the name of the school district. Ex: "Central Vermont Career Center School District"
-        - state_abbreviation (string): The abbreviation for the school district's state. Ex: Vermont --> VT
+    Inputs:
+    - username (string): The username for the student.
+    - password (string): The password for the student.
+    - district_name (string): This is the name of the school district. Ex: "Central Vermont Career Center School District"
+    - state_abbreviation (string): The abbreviation for the school district's state. Ex: Vermont --> VT
 
-        Output:
-        - Boolean: True if logging in was successful.
+    Output:
+    - Boolean: True if logging in was successful.
 
-        Operations:
-        - Inputs district_name and state_addreviation are used to request district information. 
-        - This district info is then stored and used for all future wrapper and api operations.
-        - A request is then made to log in. All cookies are stored in the session being used.
-        - Then it checks if the log in was successful, and if it is, returns True.
-        """
+    Operations:
+    - Inputs district_name and state_addreviation are used to request district information. 
+    - This district info is then stored and used for all future wrapper and api operations.
+    - A request is then made to log in. All cookies are stored in the session being used.
+    - Then it checks if the log in was successful, and if it is, returns True.
+    """
     def login(self, username: str, password: str, district_name: str, state_abbreviation: str) -> bool:
         if self.logged_in == True:
             raise InfiniteCampusExceptions.LoginExceptions.UserAlreadyLoggedInError
@@ -64,6 +81,7 @@ class InfiniteCampus():
         else:
             self.district_data = district
             self.logged_in = True
+            self.last_interaction_timestamp = time.time()
             print("Successfully logged in.")
             return True
     
@@ -85,9 +103,11 @@ class InfiniteCampus():
         def __init__(self, infinite_campus_self) -> None:
             self.infinite_campus_self: InfiniteCampus = infinite_campus_self
 
-        def fetch_self(self):
+        def fetch_student(self):
             if not self.infinite_campus_self.logged_in:
                 raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
+            
+            self.infinite_campus_self._authentication_check()
             
             district_site = self.infinite_campus_self.district_data["district_baseurl"]
             student_response = self.infinite_campus_self.session.get(f"{district_site}api/portal/students")
@@ -97,23 +117,40 @@ class InfiniteCampus():
 
             return student
 
-
         def fetch_user_account(self):
             if not self.infinite_campus_self.logged_in:
                 raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
+            
+            self.infinite_campus_self._authentication_check()
             
             district_site = self.infinite_campus_self.district_data["district_baseurl"]
             account_response = self.infinite_campus_self.session.get(f"{district_site}resources/my/userAccount")
             account_response_json = account_response.json()
             return account_response_json
             
-        def fetch_email(self):
-            if not self.infinite_campus_self.logged_in:
-                raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
-        
         def fetch_today(self):
             if not self.infinite_campus_self.logged_in:
                 raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
+            
+            self.infinite_campus_self._authentication_check()
+            
+        def fetch_attendance(self):
+            if not self.infinite_campus_self.logged_in:
+                raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
+            
+            self.infinite_campus_self._authentication_check()
+            
+            district_site = self.infinite_campus_self.district_data["district_baseurl"]
+            user_account_json = self.session.get(f"{district_site}api/portal/students").json()
+            user_account = user_account_json[0]
+
+            #I've only ever seen one enrollment. If anyone is able to experiment with more,
+            #Please, let me know or make a pull request.
+            enrollmentID = user_account["enrollments"][0]["enrollmentID"]
+
+            attendance_json = self.session.get(f"{district_site}resources/portal/attendance/{enrollmentID}?courseSummary=true").json()
+
+            return attendance_json
         
     class Grades():
         def __init__(self, infinite_campus_self) -> None:
@@ -123,15 +160,18 @@ class InfiniteCampus():
             if not self.infinite_campus_self.logged_in:
                 raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
             
+            self.infinite_campus_self._authentication_check()
+            
             district_site = self.infinite_campus_self.district_data["district_baseurl"]
             grades_json_response = self.infinite_campus_self.session.get(f"{district_site}resources/portal/grades").json()
 
-            
             return grades_json_response
         
         def fetch_grade_book_updates(self):
             if not self.infinite_campus_self.logged_in:
                 raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
+            
+            self.infinite_campus_self._authentication_check()
             
             district_site = self.infinite_campus_self.district_data["district_baseurl"]
 
@@ -173,10 +213,9 @@ class InfiniteCampus():
         def fetch_calendar(self):
             if not self.infinite_campus_self.logged_in:
                 raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
+            
+            self.infinite_campus_self._authentication_check()
 
             calendar_json = self.infinite_campus_self.session.get("https://siouxfallssd.infinitecampus.org/campus/resources/calendar/instructionalDay?calendarID=2137").json()
-
+            #https://siouxfallssd.infinitecampus.org/campus/resources/calendar/instructionalDay?trialID=824
             return calendar_json
-        
-
-        
