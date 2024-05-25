94c88faef044123fa0f3ac44616bbb625812cac0
KingTootsie <39388968+KingTootsie@users.noreply.github.com>
2024-05-24 22:33:45 -0500

    guh

---
+++ infinitecampus/schoolObjects/__init__.py
@@ -0,0 +1,6 @@
+from infinitecampus.schoolObjects.enrollment import Enrollment
+from infinitecampus.schoolObjects.term import Term
+from infinitecampus.schoolObjects.course import Course
+from infinitecampus.schoolObjects.task import Task
+from infinitecampus.schoolObjects.category import Category
+from infinitecampus.schoolObjects.assignment import Assignment
---
+++ infinitecampus/schoolObjects/assignment.py
@@ -0,0 +1,48 @@
+#https://siouxfallssd.infinitecampus.org/campus/resources/portal/section/2053248?_expand=course&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from enrollment import Enrollment
+    from term import Term
+    from course import Course
+    from category import Task
+    from category import Category
+
+class Assignment(object):
+    """Represents an assignment in an IC course."""
+    def __init__(
+        self, 
+
+        enrollment,
+        term, 
+        course, 
+        task,
+        category, 
+        
+        assignment_name: str, 
+        points_earned: float, 
+        max_points: float, 
+        not_graded: bool, 
+        late: bool, 
+        missing: bool, 
+        cheated: bool, 
+        dropped: bool
+    ) -> None:
+        self.enrollment: Enrollment = enrollment
+        self.term: Term = term
+        self.course: Course = course
+        self.task: Task = task
+        self.category: Category = category
+        
+        self.name = assignment_name
+        self.points_earned = points_earned
+        self.max_points = max_points
+        self.percent = round((self.points_earned / self.max_points) * 100, 2) if self.points_earned is not None and self.max_points is not None and self.max_points != 0 else None
+        self.not_graded = not_graded
+        self.late = late
+        self.missing = missing
+        self.cheated = cheated
+        self.dropped = dropped
+        
+    def __repr__(self) -> str:
+        return self.name
