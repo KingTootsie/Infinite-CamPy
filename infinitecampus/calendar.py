02b9ae556a9b43ce73013b5d308b364a6815e907
KingTootsie <39388968+KingTootsie@users.noreply.github.com>
2024-08-21 18:27:03 -0500

    Added 'get_day()'

--- infinitecampus/calendar.py
+++ infinitecampus/calendar.py
@@ -35,28 +35,38 @@ class Calendar():
         return day_list
         
     
-    def fetch_day(self, date: str):
-        """NOTE! RETURNS RAW JSON!"""
-        #"""date is in (YYYY-MM-DD) format"""
-
+    def get_day(self, day: int, month: int, year: int) -> Day | None:
+        """Gets a day from the calendar.
+        
+        Inputs:
+        - day (int): A specified day.
+        - month (int): A specified month.
+        - year (int): A specified year.
+        
+        Outputs:
+        - Day: Returned if specified date is a school day.
+        - None: Returned if specified date is invalid or not a school day."""
+        
         if not self.client._logged_in:
             raise InfiniteCampusExceptions.LoginExceptions.NotLoggedInError
         
         self.client.check_session_validity()
 
-        district_site = self.client.district_data["district_baseurl"]
+        day_json = self.client.http.calendar.fetch_day(day=day, month=month, year=year)
+        
+        if day_json is None:
+            return None
+        
+        day = Day(
+            date=day_json["date"],
+            is_school_day=day_json["isSchoolDay"],
+            requires_attendance=day_json["requiresAttendance"],
+            day_id=day_json["dayID"],
+            calendar_id=day_json["calendarID"],
+            structure_id=day_json["structureID"],
+            comment=day_json.get("comments")
+        )
 
-        student_info_json = self.client.session.get(f"{district_site}api/portal/students").json()
+        return day
 
-        calendar_id = student_info_json[0]["enrollments"][0]["calendarID"]
-
-        calendar_json = self.client.session.get(f"{district_site}resources/calendar/instructionalDay?calendarID={calendar_id}&date={date}").json()
-
-        is_error = "errors" in calendar_json
-
-        if is_error is True:
-            error_message = calendar_json["errors"][0]["message"]
-
-            raise InfiniteCampusExceptions.CalendarExceptions.APIException(f"The following error message was given by the Infinite Campus API:\n {error_message}")
-
-        return calendar_json
+        
--- infinitecampus/http_manager/http_calendar.py
+++ infinitecampus/http_manager/http_calendar.py
@@ -25,3 +25,27 @@ class Calendar():
             raise InfiniteCampusExceptions.CalendarExceptions.APIException(f"The following error message was given by the Infinite Campus API:\n{error_message}\nPlease report this on the issues tab at https://github.com/KingTootsie/Infinite-CamPy/issues")
         
         return calendar_json
+    
+    def fetch_day(self, day: int, month: int, year: int):
+        district_site = self.http.district_data["district_baseurl"]
+
+        student_info_json = self.http.session.get(f"{district_site}api/portal/students").json()
+
+        calendar_id = student_info_json[0]["enrollments"][0]["calendarID"]
+
+        calendar_json = self.http.session.get(f"{district_site}resources/calendar/instructionalDay?calendarID={calendar_id}&date={year}-{month}-{day}").json()
+        print(f"{district_site}resources/calendar/instructionalDay?calendarID={calendar_id}&date={year}-{month}-{day}")
+
+        is_error = "errors" in calendar_json
+
+        if is_error is True:
+            error_message = calendar_json["errors"][0]["message"]
+
+            raise InfiniteCampusExceptions.CalendarExceptions.APIException(f"The following error message was given by the Infinite Campus API:\n {error_message}")
+
+        try:
+            return calendar_json[0]
+        except IndexError:
+            return None
+    
+        
